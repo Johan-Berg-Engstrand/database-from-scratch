6a0f6bc1ea3bb8a9516d38d24645ebffa4364af7
Johan Berg <119810021+Johan-Berg-Engstrand@users.noreply.github.com>
2025-05-18 15:30:40 +0200

    Add b-tree node functions

---
+++ btree/btree.go
@@ -0,0 +1,30 @@
+package btree
+
+import "encoding/binary"
+
+type BNode []byte
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := 4 + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+func assert(cond bool) {
+	if (!cond) {
+		panic("assertion failure")
+	}
+}
